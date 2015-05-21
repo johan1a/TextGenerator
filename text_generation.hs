93658fc92c56800c42e14d9ae98606989e451375
= <=>
2015-05-22 00:27:49 +0200

    refactor

--- text_generation.hs
+++ text_generation.hs
@@ -14,8 +14,7 @@ type SuffixFrequencyMap = Map Bigram WordFrequencyMap
 
 main = do
 		wordList <- getWords "jeeves.txt"
-		let suffixMap = suffixFrequencies wordList
-		inputLoop suffixMap
+		inputLoop $ suffixFrequencies wordList
 
 
 inputLoop suffixMap = do
@@ -39,15 +38,15 @@ generateText suffixMap wordCount = do
 generateText_ :: SuffixFrequencyMap -> Int -> Bigram -> IO String
 generateText_ suffixMap 0 bigram = return("")
 generateText_ suffixMap wordsLeft bigram = do
-	nextWord <- randomSuffix (Map.findWithDefault Map.empty bigram suffixMap)
+	nextWord <- randomSuffix $ Map.findWithDefault Map.empty bigram suffixMap
 	text <- generateText_ suffixMap (wordsLeft - 1) (snd bigram, nextWord)
 	return $ fst bigram ++ " " ++ text
 	
 randomBigram :: SuffixFrequencyMap -> IO Bigram
-randomBigram suffixFrequencyMap = pick (Map.keys suffixFrequencyMap)
+randomBigram suffixFrequencyMap = pick $ Map.keys suffixFrequencyMap
 
 randomSuffix :: WordFrequencyMap -> IO String
-randomSuffix wordFrequencies = pick (Map.keys wordFrequencies)
+randomSuffix wordFrequencies = pick $ Map.keys wordFrequencies
 
 mostProbableSuffix :: WordFrequencyMap -> String
 mostProbableSuffix wordFrequencies = keyWithMaxValFromMap wordFrequencies
@@ -55,7 +54,7 @@ mostProbableSuffix wordFrequencies = keyWithMaxValFromMap wordFrequencies
 getWords :: FilePath -> IO [String]
 getWords filePath = do 
 					contents <- readFile filePath
-					return (words contents)
+					return $ words contents
 
 suffixFrequencies :: [String] -> SuffixFrequencyMap
 suffixFrequencies xs = suffixFrequencies_ xs Map.empty
@@ -63,23 +62,22 @@ suffixFrequencies xs = suffixFrequencies_ xs Map.empty
 suffixFrequencies_ :: [String] -> SuffixFrequencyMap -> SuffixFrequencyMap
 suffixFrequencies_ [] m = m
 suffixFrequencies_ [a, b] m = m
-suffixFrequencies_ (x:xs) m = mergeSFM m (suffixFrequencies_ xs (singleSuffix (x:xs)))
+suffixFrequencies_ (x:xs) m = mergeSFM m $ suffixFrequencies_ xs $ singleSuffix (x:xs)
 
 singleSuffix :: [String] -> SuffixFrequencyMap
-singleSuffix xs = Map.fromList [((headTuple xs), makeWFM (xs !! 2))]
+singleSuffix xs = Map.fromList [(headTuple xs, makeWFM $ xs !! 2)]
 
 makeWFM :: String -> WordFrequencyMap
 makeWFM s = Map.fromList[(s, 1)]
 
 mergeSFM :: SuffixFrequencyMap -> SuffixFrequencyMap -> SuffixFrequencyMap
-mergeSFM a b = Map.unionWith mergeWFM a b
+mergeSFM = Map.unionWith mergeWFM
 
 mergeWFM :: WordFrequencyMap -> WordFrequencyMap -> WordFrequencyMap
-mergeWFM a b = Map.unionWith (+) a b
+mergeWFM = Map.unionWith (+) 
 
 headTuple :: [String] -> Bigram
 headTuple xs = (xs !! 0, xs !! 1) 
-
 
 
 pick :: [a] -> IO a
